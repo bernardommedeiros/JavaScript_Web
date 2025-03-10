0948511de62ed9213da46dd429df2a4b4eea78e2
Bernardo de Moura Medeiros <161090624+bernardommedeiros@users.noreply.github.com>
2025-03-10 22:59:47 +0000

    declaracao de variaveis

---
+++ Conceitos_Iniciais/Declaracoes/js/index.js
@@ -0,0 +1,24 @@
+// declaração de variáveis
+// ; não necessário mas recomendável
+
+let a, b ,c;
+a = 5;
+b = 6;
+c = 7;
+
+let pessoa = "Bernardo";
+
+
+/*
+
+- comentário de múltiplas linhas.
+
+*/
+
+// função - declaração em bloco.
+
+function minhaFuncao(){
+    document.writeln (5 + 5);
+    document.writeln (4 + 4);
+
+}
