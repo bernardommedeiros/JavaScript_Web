e4fe8c4baf040f17ef19e1e25a518415dc7da08c
bernardommedeiros <161090624+bernardommedeiros@users.noreply.github.com>
2025-03-16 04:15:11 -0300

    setTimeout

--- Eventos_de_TempoJS/eventos__de_tempo.md
+++ Eventos_de_TempoJS/eventos__de_tempo.md
@@ -5,7 +5,7 @@
 
 ## Métodos de cronometragem
 
-- ```setTimeout(function, tempo em milissegundos)```: executa uma função, depois de atingir o número especificado de milissegundos.
+- ```setTimeout(function, tempo em milissegundos)```: executa a função apenas uma vez, depois de atingir o tempo determinado.
  
-- ```setInterval(function, milliseconds)```: mesma função do setTimeout(), mas repete a execução de forma contínua.
+- ```setInterval(function, milliseconds)```: mesma funcionalidade do setTimeout(), mas repete a execução de forma contínua.
   
---
+++ Eventos_de_TempoJS/js/index.js
@@ -0,0 +1,18 @@
+// Métodos de Tempo JavaScript
+
+function ativarContagem(){
+    document.getElementById('tempo').innerHTML = "começou a contar!";
+    //setTimeout(function, tempo em milissegundos)
+    setTimeout(function(){ document.getElementById('tempo').innerHTML = "Executou o setTimeout" }, 5000)
+    setTimeout(function(){ document.body.style.backgroundColor = "blue";}, 6000)
+    setTimeout(function() {
+        if (document.body.style.backgroundColor === "blue" || document.body.style.backgroundColor === "rgb(0, 0, 255)") {
+            document.getElementById('bgcolor').innerHTML = "O plano de fundo foi alterado!";
+        }
+    }, 100);
+}
+
+function pausarContagem(){
+    clearTimeout();
+    document.getElementById('tempo').innerHTML = "pausou a contagem!";
+}
