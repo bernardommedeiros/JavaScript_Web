601bc860f9f0fce0976da3f26c668842af6d5d1e
bernardommedeiros <161090624+bernardommedeiros@users.noreply.github.com>
2025-03-20 00:37:40 -0300

    continuação calculadora

--- Projeto-Calculadora/src/scripts/calculadora.js
+++ Projeto-Calculadora/src/scripts/calculadora.js
@@ -1,16 +1,20 @@
-// function insertNumber(num){
-//     console.log(num);
-// }
-
+// INSERÇÂO NO DISPLAY
 function insertElement(element){
-    const display = document.getElementById('display').value += element;
+    document.getElementById('display').value += element;
 }
 
+// REMOVE
 function removeElement(){
-    
+    const remove = document.getElementById('display').value;
+    document.getElementById('display').value = remove.slice(0, -1);
 }
 
-
+// AC
 function clearDisplay(){
     document.getElementById('display').value = '';
+}
+
+// CALCULO DOS ELEMENTOS DO DISPLAY APÓS REQUISIÇÂO DE GERAR RESULTADO
+function calc(){
+    const displayElements = document.getElementById('display').value;
 }
