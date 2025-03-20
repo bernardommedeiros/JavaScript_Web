bdd0c8c63e1bea296080f86bfe3bbd1bbfbf4866
bernardommedeiros <161090624+bernardommedeiros@users.noreply.github.com>
2025-03-20 14:09:11 -0300

    logica da calculadora quase finalizada

--- Projeto-Calculadora/src/scripts/calculadora.js
+++ Projeto-Calculadora/src/scripts/calculadora.js
@@ -26,10 +26,23 @@ function percentage() {
     
 }
 
-
 function not() {
-    const displayNum = document.getElementById('display').value;
-    document.getElementById('display').value = displayNum * -1;
+    function calc(){
+        const displayElements = document.getElementById('display').value;
+    
+        document.getElementById('display').value = eval(displayElements);
+    }
+
+    const displayElements = document.getElementById('display').value;
+
+    if (displayElements.startsWith("-")){
+        document.getElementById('display').value = displayElements.slice(1);
+    } 
+    else if (displayElements.startsWith("1") || displayElements.startsWith("2") || displayElements.startsWith("3") || displayElements.startsWith("4") || displayElements.startsWith("5") || displayElements.startsWith("6") || displayElements.startsWith("7") || displayElements.startsWith("8") || displayElements.startsWith("9")){
+        document.getElementById('display').value = "-" + displayElements;
+    }
+
 }
+
 
 
