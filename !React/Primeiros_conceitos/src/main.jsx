0a611da32569b2bdf0ae3672772a9f2ac89fef3e
bernardommedeiros <161090624+bernardommedeiros@users.noreply.github.com>
2025-03-21 19:10:24 -0300

    inicialização primeiro componente

--- !React/Primeiros_conceitos/src/main.jsx
+++ !React/Primeiros_conceitos/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// renderização do componente react na div root
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
