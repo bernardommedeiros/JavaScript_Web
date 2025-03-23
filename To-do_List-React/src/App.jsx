51906f7052871435f54853a7506b31f954068e57
bernardommedeiros <161090624+bernardommedeiros@users.noreply.github.com>
2025-03-22 21:29:28 -0300

    continuação componente

--- To-do_List-React/src/App.jsx
+++ To-do_List-React/src/App.jsx
@@ -1,14 +1,17 @@
-import Tasks from "./components/Tasks"
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask"
 
 function App() {
 
   return (
     <div>
       <h1> Gerenciador de Tarefas </h1>
+      <AddTask />
+      <Tasks />
     </div>
-    
-    
-  )
+
+
+  );
 }
 
-export default App
+export default App;
