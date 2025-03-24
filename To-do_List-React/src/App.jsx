c83d63639081eb1d790e213930ada3e4d62a2ae6
bernardommedeiros <161090624+bernardommedeiros@users.noreply.github.com>
2025-03-24 18:52:50 -0300

    tailwind initial

--- To-do_List-React/src/App.jsx
+++ To-do_List-React/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div>
-      <h1> Gerenciador de Tarefas </h1>
+      <h1 className="text-red-500 text-3xl"> Gerenciador de Tarefas </h1>
       <AddTask />
       <Tasks />
     </div>
